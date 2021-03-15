cbfc74598b37b6719128074d087032934ae7f3b9
francesco.chiapponi <chiapponifrancesco98@gmail.com>
2021-03-15 10:38:44 +0100

    some comments and an attempt to chose between pf1 and pf2

--- pb3.py
+++ pb3.py
@@ -11,27 +11,33 @@ import numpy as np
 
 def max_prime_fact(N):
     
-    f=[]
+    d=[]
     
-    for number in range(N):
+    for number in range(N):     #create an array of divisors of N
         if N%(number+1)==0:
-            f=f+[number+1]
+            d=d+[number+1]
 
-    factor=np.array(f)
+    div=np.array(d)
 
-    for l in range(len(f)):
-        if all(factor[l]%factor[1:l])!=0:
-            PF=factor[l];
+    for l in range(len(d)):         #only the prime factors are selected 
+        if all(div[l]%div[1:l])!=0:
+            PF=div[l];
     return PF
 
-N=13195#600851475143
+N=600851475143
 rad=int(np.sqrt(N))
 
-while N%rad!=0:
+while N%rad!=0:                 #factorization of N into 2 smaller numbers
     rad=rad+1
 
 f1=rad
 f2=int(N/rad)
 
-pf1=max_prime_fact(f1)
+pf1=max_prime_fact(f1)          #calculate the max prime factor of each number
 pf2=max_prime_fact(f2)
+
+if pf2%pf1==0:
+    maxPF=pf1
+else maxPF=max(pf1,pf2)
+
+print(maxPF)
